94ab83ac05d7c0cf73cc9932fdcfbdb7a73ae801
Viet Tran Quoc Hoang <hoangviet1993@gmail.com>
2020-04-18 21:22:18 -0700

    Remove method to receive form post and remove catching exception and move to db file

--- URLShortenerAPI/Controllers/LinksController.cs
+++ URLShortenerAPI/Controllers/LinksController.cs
@@ -8,7 +8,6 @@ using System.Web;
 using System.Web.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
 
 namespace URLShortenerAPI.Controllers
 {
@@ -16,30 +15,31 @@ namespace URLShortenerAPI.Controllers
     [ApiController]
     public class LinksController : ControllerBase
     {
-        // GET api/links/abc123
+        // GET api/APIReady
         [HttpGet("APIReady")]
         public IActionResult Get()
         {
             return Content("API is Ready");
         }
+
         // GET api/links/abc123
         [HttpGet("{hashID}")]
         [ProducesResponseType(StatusCodes.Status301MovedPermanently)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Get(string hashID)
         {
-            try
-            {
-                Models.ShortLink hashIDItem = await HashIDDBRepository.GetHashIDItemAsync(hashID);
-                return Redirect(hashIDItem.Url);
-            }
-            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            Models.ShortLink hashIDItem = await HashIDDBRepository.GetHashIDItemAsync(hashID);
+            if (hashIDItem == null)
             {
                 Models.ErrorResponse notFoundResponse = new Models.ErrorResponse
                 {
                     Details = "Not found."
                 };
                 return NotFound(notFoundResponse);
+            }
+            else
+            {
+                return Redirect(hashIDItem.Url);
             }
         }
 
@@ -49,7 +49,7 @@ namespace URLShortenerAPI.Controllers
         [Consumes("application/json")]
         public async Task<IActionResult> CreateHashIDPostedJson([FromBody] Models.UrlString longUrl)
         {
-            if (URLValidator.IsUrlValid(longUrl.url))
+            if (ModelState.IsValid && URLValidator.IsUrlValid(longUrl.url))
             {
                 Models.ShortLink hashIDItem = await HashIDDBRepository.CreateHashIDItemIfNotExistsAsync(longUrl.url);
                 Models.PublicShortLink response = new Models.PublicShortLink
@@ -67,33 +67,6 @@ namespace URLShortenerAPI.Controllers
                     Details = "Enter a valid URL."
                 };
                 return BadRequest(invalidURLResponse);
-            }
-        }
-
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [Consumes("application/x-www-form-urlencoded")]
-        public async Task<IActionResult> CreateHashIDPostedForm([FromForm] Models.UrlString longUrl)
-        {
-            if (URLValidator.IsUrlValid(longUrl.url))
-            {
-                Models.ShortLink hashIDItem = await HashIDDBRepository.CreateHashIDItemIfNotExistsAsync(longUrl.url);
-                Models.PublicShortLink response = new Models.PublicShortLink
-                {
-                    Url = hashIDItem.Url,
-                    HashID = hashIDItem.HashID,
-                    CreatedTime = hashIDItem.CreatedTime
-                };
-                return Ok(response);
-            }
-            else
-            {
-                Models.ErrorResponse errorResponse = new Models.ErrorResponse
-                {
-                    Details = "Enter a valid URL."
-                };
-                return BadRequest(errorResponse);
             }
         }
     }
